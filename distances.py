10455f7d9875aae44b7c3235a1bb126a207deec8
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-07-19 16:09:23 +0100

    first pass at distance calculations

---
+++ distances.py
@@ -0,0 +1,25 @@
+from numpy import e, sqrt
+from scipy.integrate import quad
+import flexknot
+
+w = flexknot.FlexKnot(0, 1)
+
+
+def integrand(z, theta):
+    return (1 + w(1/(1+z), theta)) / (1 + z)
+
+
+def f_de(z, theta):
+    integral = quad(lambda z: (1 + w(1/(1+z), theta)) / (1 + z), 0, z)
+    return e**(3*integral)
+
+
+def h(z, omegam, omegar, theta):
+    """
+    H(z) / H0
+    """
+    return sqrt(
+        omegam / (1 + z)**3
+        + omegar / (1 + z)**4
+        + (1 - omegam - omegar) * f_de(z, theta)
+    )
