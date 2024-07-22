f5641ccaf25883e326dcf090fbe30a5ab3a8f6d0
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-07-22 12:25:52 +0100

    changes for more complex flexknots

--- run.py
+++ run.py
@@ -1,16 +1,32 @@
+import sys
 import numpy as np
 import pypolychord
 from pypolychord.priors import UniformPrior
-from likelihood import likelihood
 from flexknot import Prior
+from anesthetic import make_2d_axes
+from likelihood import likelihood
+from mpi4py import MPI
 
-ndims = 3
+comm = MPI.COMM_WORLD
+
+
+n = int(sys.argv[1])
 
 paramnames = [
     (r"H0rd", r"H_0r_d"),
     (r"Omegam", r"\Omega_\mathrm{m}"),
-    (r"w0", r"w_0")
+    ("wn", "w_n")
 ]
+
+for i in range(0, n-2, -1):
+    paramnames += [
+        (f"a{i}", f"a_{i}"),
+        (f"w{i}", f"w_{i}"),
+    ]
+paramnames += [("wn", "w_n")]
+
+ndims = len(paramnames)
+params = [paramname[0] for paramname in paramnames]
 
 h0rd_prior = UniformPrior(3650, 18250)
 omegam_prior = UniformPrior(0.01, 0.99)
@@ -29,5 +45,14 @@ x = np.random.rand(ndims)
 
 likelihood(prior(x))
 
-pypolychord.run(likelihood, ndims, prior=prior,
-                paramnames=paramnames)
+file_root = f"test{n}"
+
+ns = pypolychord.run(likelihood, ndims, prior=prior,
+                     nlive=500,
+                     paramnames=paramnames,
+                     file_root=file_root,
+                     read_resume=False)
+if comm.rank == 0:
+    fig, axes = make_2d_axes(params)
+    ns.plot_2d(axes)
+    fig.save(f"plots/{file_root}.pdf", bbox_inches='tight')
