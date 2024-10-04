615f717eb4670035fac004a012b094e3dfe82488
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-10-04 17:28:38 +0100

    tidy up nocepheid.py

--- nocepheid.py
+++ nocepheid.py
@@ -3,15 +3,11 @@ import numpy as np
 from numpy import array, log10
 import pandas as pd
 from scipy.stats import multivariate_normal
-import scipy
 import pypolychord
 from distances import dl
 from anesthetic import make_2d_axes
-from mpi4py import MPI
 from clustering import xmeans
 from flexknot import Prior
-
-comm = MPI.COMM_WORLD
 
 
 # data loading stolen from Toby
@@ -19,11 +15,9 @@ df = pd.read_table('../clik_installs/desi/data/sn_data/PantheonPlus/Pantheon+SH0
 cov = np.reshape(np.loadtxt('../clik_installs/desi/data/sn_data/PantheonPlus/Pantheon+SH0ES_STAT+SYS.cov', skiprows=1), [1701, 1701])
 
 mask = (df['zHD'] > 0.023) | (df['IS_CALIBRATOR'] == 1)
-iamask = (df['IS_CALIBRATOR'] == 0)[mask]
 
 mbcorr = df['m_b_corr'].to_numpy()[mask]
 
-z = df['zHD'].to_numpy()[mask]
 zhd = df['zHD'].to_numpy()[mask]
 zhel = df['zHEL'].to_numpy()[mask]
 
@@ -33,65 +27,25 @@ mcov = cov[mask, :][:, mask]
 
 gaussian = multivariate_normal(np.zeros(len(mcov)), mcov)
 
-mu2 = mbcorr - cephdist
-cephmask = 1 - iamask
-mu2masked = mu2 * cephmask
-
-
-def ia_likelihood(Mb, h0, omegam, omegar, theta):
-    theta = array(theta)
-
-    # dl is calculated in Mpc = [c/H0]
-    # mu = 5 log(dl/10pc), so 25 comes from converting from Mpc to pc
-    mu = 5 * log10(dl(z, h0, omegam, omegar, theta)) + 25
-    mu1 = mbcorr - mu
-
-    mu = mu1
-    return gaussian.logpdf(Mb - mu)
-
-
-def corrected_ia_likelihood(Mb, h0, omegam, omegar, theta):
-    theta = array(theta)
-
-    # dl is calculated in Mpc = [c/H0]
-    # mu = 5 log(dl/10pc), so 25 comes from converting from Mpc to pc
-    mu = 5 * log10(dl(zhd, zhel, h0, omegam, omegar, theta)) + 25
-    mu1 = mbcorr - mu
-
-    mu = mu1
-    return gaussian.logpdf(Mb - mu)
-
 
 one = np.ones(len(mcov))[:, None]
 invcov = np.linalg.inv(mcov)
 invcov_tilde = invcov - invcov @ one @ one.T @ invcov / (one.T @ invcov @ one)
-print(f"{mcov=}")
-print(f"{invcov_tilde.shape=}")
-print(f"{np.linalg.det(mcov)=}")
-print(f"{scipy.linalg.det(mcov)=}")
-print(f"{np.linalg.slogdet(mcov)=}")
-print(f"{one.T @ invcov @ one=}")
-lognormalisation = float(-0.5 * np.log(one.T @ invcov @ one))
-print(f"{lognormalisation=}")
 lognormalisation = 0.5 * (np.log(2*np.pi)
                           - np.linalg.slogdet(2 * np.pi * mcov)[1]
                           - float(np.log(one.T @ invcov @ one)))
-print(f"{lognormalisation=}")
 
 
-def marginalised(h0, omegam, omegar, theta):
+def logl_ia(h0, omegam, omegar, theta=np.array([-1])):
     theta = np.array(theta)
-    mu = 5 * log10(dl(z, h0, omegam, omegar, theta)) + 25
-    mu1 = mbcorr - mu
+    mu = 5 * log10(dl(zhd, zhel, h0, omegam, omegar, theta)) + 25
+    x = mbcorr - mu
 
-    mu = (mu1 * iamask + mu2masked).to_numpy()[:, None]
-    return lognormalisation + float(-mu.T @ invcov_tilde @ mu / 2)
+    return lognormalisation + float(-x.T @ invcov_tilde @ x / 2)
 
 
-lower = array([-20, 20, 0.01])
-upper = array([-18, 100, 0.99])
-# lower = array([20, 0.01])
-# upper = array([100, 0.99])
+lower = array([20, 0.01])
+upper = array([100, 0.99])
 prior_range = upper - lower
 
 flexknotprior = Prior(0, 1, -3, -0.01)
@@ -108,12 +62,14 @@ omegar = 8.24e-5
 
 
 def likelihood(theta):
-    Mb, h0, omegam, *theta = theta
-    return ia_likelihood(Mb, h0, omegam, omegar, theta)
-    # return marginalised(h0, omegam, omegar, theta)
+    h0, omegam, *theta = theta
+    return logl_ia(h0, omegam, omegar, theta)
 
 
 if __name__ == "__main__":
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+
     try:
         n = int(sys.argv[1])
     except ValueError:
@@ -122,12 +78,7 @@ if __name__ == "__main__":
         def prior(x):
             return lower + x * prior_range
 
-        def likelihood(theta):
-            Mb, h0, omegam = theta
-            return ia_likelihood(Mb, h0, omegam, omegar, np.array([-1]))
-
     paramnames = [
-        ("Mb", r"M_\mathrm{b}"),
         ("H0", r"H_0"),
         ("Omegam", r"\Omega_\mathrm{m}"),
     ]
@@ -145,15 +96,10 @@ if __name__ == "__main__":
             paramnames += [("w0", "w_0")]
 
     ndims = len(paramnames)
-    file_root = f"nocepheid_{n}"
-
-    for i in range(1):
-        print(i)
-        print(prior(np.random.rand(ndims)))
-        print(likelihood(prior(np.random.rand(ndims))))
+    file_root = f"ia_{n}"
 
     ns = pypolychord.run(likelihood, ndims, prior=prior,
-                         read_resume=False,
+                         read_resume=True,
                          paramnames=paramnames,
                          file_root=file_root,
                          nlive=1000,
