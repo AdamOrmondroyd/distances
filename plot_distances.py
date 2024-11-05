a6f7f4af206ef27448ac8f049f9b27e8180f88f0
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-11-05 16:13:12 +0000

    add flexknot to distances plot

--- plot_distances.py
+++ plot_distances.py
@@ -6,6 +6,8 @@ from anesthetic.samples import merge_samples_weighted
 from fgivenx import plot_lines
 from distances import dh_over_rs, dm_over_rs, dv_over_rs
 from bao import dmdhplot, dvplot
+from flexknot import FlexKnot
+import smplotlib
 
 
 name = sys.argv[1]
@@ -69,7 +71,7 @@ def dv_over_rdz23(z, theta):
     ]
 
 
-fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+fig, axs = plt.subplots(3, figsize=(6, 14))
 z = np.linspace(0.01, 2.5)
 plot_lines(dm_over_zdh, z, ns[params], weights=ns.get_weights(), ax=axs[0],
            cache=f"cache/{name}_dm_over_zdh_{n}{'_i' if single else ''}",
@@ -77,10 +79,22 @@ plot_lines(dm_over_zdh, z, ns[params], weights=ns.get_weights(), ax=axs[0],
 plot_lines(dv_over_rdz23, z, ns[params], weights=ns.get_weights(), ax=axs[1],
            cache=f"cache/{name}_dv_over_rdz23_{n}{'_i' if single else ''}",
            parallel=True)
+
+fk = FlexKnot(0, 1)
+
+
+def fz(z, theta):
+    theta = theta[~np.isnan(theta)]
+    return fk(1/(1+z), theta)
+
+
+plot_lines(fz, z, ns[params[2:]], weights=ns.get_weights(), ax=axs[2])
+
 for ax in axs:
     ax.set(xlim=(0, 2.5))
 dmdhplot(axs[0])
 dvplot(axs[1])
+axs[1].legend()
 # axs[0].set(ylim=(17, 21.5))
 # axs[1].set(ylim=(0.95, 2.1))
 
