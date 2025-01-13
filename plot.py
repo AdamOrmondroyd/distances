3a90af2fe4fc0dc31ffe6e9d186ae5a9d2519a19
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-01-13 11:52:42 +0000

    save plot.py output to plots/name, plus some tidying

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 import numpy as np
 from matplotlib import pyplot as plt
 import smplotlib
@@ -37,6 +38,8 @@ else:
     ns = merge_samples_weighted(nss, weights=[pc.logZ for pc in pcs])
 
 if __name__ == "__main__":
+    prior_color = 'C0'
+    post_color = 'C1'
     fig, ax = plt.subplots(2, 2, figsize=(12, 12))
     ax = ax.flatten()
 
@@ -49,18 +52,16 @@ if __name__ == "__main__":
     x = np.linspace(0, 1, 100)
     print(f"{ns[params]=}")
     plot_lines(f, x, prior[params], weights=prior.get_weights(),
-               ax=ax[0], color='C1')
+               ax=ax[0], color=prior_color)
     plot_lines(f, x, ns[params], weights=ns.get_weights(),
-               ax=ax[0], color='C0')
-    # plot_contours(f, x, ns[params], weights=ns.get_weights(), ax=ax[1])
-    for axi in ax[0], :  # ax[1]:
-        axi.set(xlabel="$a$", ylabel="$w(a)$",
-                xlim=(0, 1), ylim=(-3, 0))
+               ax=ax[0], color=post_color)
+    ax[0].set(xlabel="$a$", ylabel="$w(a)$",
+              xlim=(0, 1), ylim=(-3, 0))
     col = 'H0' if 'H0' in ns else 'H0rd'
     prior[col].plot.hist_1d(bins=40, ax=ax[1], alpha=0.5,
-                            label='prior', color='C1')
+                            label='prior', color=prior_color)
     ns[col].plot.hist_1d(bins=40, ax=ax[1], alpha=0.5,
-                         label='posterior', color='C0')
+                         label='posterior', color=post_color)
     ax[1].set(xlabel=ns.get_label(col),
               xlim=(20, 100) if col == 'H0' else (3650, 18250), ylim=(0, 1.35))
     ax[1].legend(bbox_to_anchor=(1.05, 1), loc='upper right')
@@ -93,15 +94,17 @@ if __name__ == "__main__":
 
     z = np.logspace(-3, np.log10(2.5))
     plot_lines(fz, z, prior[params], weights=prior.get_weights(),
-               ax=ax[3], color='C1')
+               ax=ax[3], color=prior_color)
     plot_lines(fz, z, ns[params], weights=ns.get_weights(),
-               ax=ax[3], color='C0')
+               ax=ax[3], color=post_color)
     ax[3].set(xlabel="$z$", ylabel="$w(z)$",
               xlim=(min(z), max(z)), ylim=(-3, 0),
               xscale='log')
 
     fig.suptitle(f"{name}_{n}{'_i' if single else ''}")
     fig.tight_layout()
-    fig.savefig(f"plots/{name}_{n}{'_i' if single else ''}_wa.pdf",
+    plotpath = Path("plots") / name
+    plotpath.mkdir(parents=True, exist_ok=True)
+    fig.savefig(plotpath / f"{name}_{n}{'_i' if single else ''}_wa.pdf",
                 bbox_inches='tight')
     plt.show()
