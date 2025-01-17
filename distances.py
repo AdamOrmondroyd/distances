817b11a3e83c17f50c416336f4c1c74ac4c50295
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-01-17 14:48:50 +0000

    switch to analytic integration in f_de

--- distances.py
+++ distances.py
@@ -24,6 +24,51 @@ def f_de_redshift(z, theta):
     return e**(3*integral)
 
 
+def f_de_redshift_analytic(z, theta):
+    # WARN: I think the right_x_nodes are wrong
+    x_nodes = get_x_nodes_from_theta(theta, False)
+    y_nodes = get_y_nodes_from_theta(theta, False)
+    left_x_nodes = concatenate([[0], x_nodes[x_nodes < z]])
+    limits = concatenate([left_x_nodes, [z]])
+    right_x_nodes = x_nodes[1:len(left_x_nodes)+1]
+    left_y_nodes = y_nodes[:len(left_x_nodes)]
+    right_y_nodes = y_nodes[1:len(left_x_nodes)+1]
+    m = (right_y_nodes - left_y_nodes) / (right_x_nodes - left_x_nodes)
+    c = left_y_nodes - m * left_x_nodes
+    assert np.allclose(c, (left_y_nodes * right_x_nodes - right_y_nodes * left_x_nodes) / (right_x_nodes - left_x_nodes))
+
+    one_plus_c_minus_m = 1 + c - m
+    integrals = (
+        one_plus_c_minus_m * log(1 + limits[1:]) + m * limits[1:]
+        - one_plus_c_minus_m * log(1 + limits[:-1]) - m * limits[:-1]
+    )
+    return e**(3*sum(integrals))
+
+
+def f_de_analytic(z, theta):
+    a = 1 / (1 + z)
+    ai = get_x_nodes_from_theta(theta, False)[::-1]
+    wi = get_y_nodes_from_theta(theta, False)[::-1]
+    # NOTE: "left" means the lower integration limit. a0 = 1
+    ai = concatenate([[1], ai, [0]])
+    left_a = ai[ai > a]  # aka ai
+    limits = concatenate([left_a, [a]])
+    right_a = ai[1:len(left_a)+1]  # aka ai+1
+    left_w = wi[:len(left_a)]  # aka wi
+    right_w = wi[1:len(right_a)+1]  # aka wi+1
+
+    m = (left_w - right_w) / (left_a - right_a)
+    one_plus_c = left_w - m * left_a + 1
+    assert np.allclose(one_plus_c, 1+(left_w * right_a - right_w * left_a) / (right_a - left_a))
+
+    uppers = -one_plus_c * log(limits[1:]) - m * limits[1:]
+    lowers = -one_plus_c * log(limits[:-1]) - m * limits[:-1]
+    integrals = uppers - lowers
+
+    result = e**(3*sum(integrals))
+    return result
+
+
 def f_de(z, theta):
     # split integral by flexknot nodes
     x_nodes = get_x_nodes_from_theta(theta, False)
@@ -48,12 +93,12 @@ def h(z, omegam, omegar, theta, redshift=False):
         return sqrt(
             omegam * (1 + z)**3
             + omegar * (1 + z)**4
-            + (1 - omegam - omegar) * f_de_redshift(z, theta)
+            + (1 - omegam - omegar) * f_de_redshift_analytic(z, theta)
         )
     return sqrt(
         omegam * (1 + z)**3
         + omegar * (1 + z)**4
-        + (1 - omegam - omegar) * f_de(z, theta)
+        + (1 - omegam - omegar) * f_de_analytic(z, theta)
     )
 
 
