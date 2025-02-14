049c31315fef90599427c9455719555a3c4b582e
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-02-14 11:25:28 +0000

    remove redshift nonsense

--- common.py
+++ common.py
@@ -9,10 +9,9 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 
 flexknotprior = Prior(0, 1, -3, -0.01)
-flexknotprior_redshift = Prior(0, 3, -3, -0.01)
 
 
-def flexknotparamnames(n, redshift=False, tex=True):
+def flexknotparamnames(n, tex=True):
     try:
         n = int(n)
     except ValueError:
@@ -23,7 +22,7 @@ def flexknotparamnames(n, redshift=False, tex=True):
 
     for i in range(n-2, 0, -1):
         p += [
-            (f"z{i}", f"z_{{{i}}}") if redshift else (f"a{i}", f"a_{{{i}}}"),
+            (f"a{i}", f"a_{{{i}}}"),
             (f"w{i}", f"w_{{{i}}}"),
         ]
     if n >= 1:
@@ -34,15 +33,15 @@ def flexknotparamnames(n, redshift=False, tex=True):
 
 
 def run(likelihood, n, priors,
-        file_root, paramnames, read_resume, redshift=False):
-    paramnames += flexknotparamnames(n, redshift)
+        file_root, paramnames, read_resume):
+    paramnames += flexknotparamnames(n)
     try:
         n = int(n)
 
         def prior(x):
             return np.concatenate([
                 [prior(xi) for xi, prior in zip(x, priors)],
-                flexknotprior_redshift(x[len(priors):]) if redshift else flexknotprior(x[len(priors):])
+                flexknotprior(x[len(priors):])
             ])
     except ValueError:
         def prior(x):
--- distances.py
+++ distances.py
@@ -9,38 +9,6 @@ from flexknot.utils import get_x_nodes_from_theta, get_y_nodes_from_theta
 c = c/1000
 
 w = FlexKnot(0, 1)
-wz = FlexKnot(0, 3)
-
-
-def f_de_redshift(z, theta):
-    x_nodes = get_x_nodes_from_theta(theta, False)
-    x_nodes = x_nodes[x_nodes < z]
-    limits = concatenate([[0], x_nodes, [z]])
-    integral = sum([
-        quad(lambda z: (1 + wz(z, theta)) / (1 + z), lower, upper)[0]
-        for lower, upper in zip(limits[:-1], limits[1:])
-    ])
-    return e**(3*integral)
-
-
-def f_de_redshift_analytic(z, theta):
-    # WARN: I think the right_x_nodes are wrong
-    x_nodes = get_x_nodes_from_theta(theta, False)
-    y_nodes = get_y_nodes_from_theta(theta, False)
-    left_x_nodes = concatenate([[0], x_nodes[x_nodes < z]])
-    limits = concatenate([left_x_nodes, [z]])
-    right_x_nodes = x_nodes[1:len(left_x_nodes)+1]
-    left_y_nodes = y_nodes[:len(left_x_nodes)]
-    right_y_nodes = y_nodes[1:len(left_x_nodes)+1]
-    m = (right_y_nodes - left_y_nodes) / (right_x_nodes - left_x_nodes)
-    c = left_y_nodes - m * left_x_nodes
-
-    one_plus_c_minus_m = 1 + c - m
-    integrals = (
-        one_plus_c_minus_m * log(1 + limits[1:]) + m * limits[1:]
-        - one_plus_c_minus_m * log(1 + limits[:-1]) - m * limits[:-1]
-    )
-    return e**(3*sum(integrals))
 
 
 def f_de_analytic(z, theta):
@@ -82,16 +50,10 @@ def f_de(z, theta):
     return e**(3*integral)
 
 
-def h(z, omegam, omegar, theta, redshift=False):
+def h(z, omegam, omegar, theta):
     """
     H(z) / H0
     """
-    if redshift:
-        return sqrt(
-            omegam * (1 + z)**3
-            + omegar * (1 + z)**4
-            + (1 - omegam - omegar) * f_de_redshift_analytic(z, theta)
-        )
     return sqrt(
         omegam * (1 + z)**3
         + omegar * (1 + z)**4
@@ -101,38 +63,39 @@ def h(z, omegam, omegar, theta, redshift=False):
 
 # TODO: check units of everything - c is km/s!
 
-def h0dh(z, omegam, omegar, theta, redshift=False):
-    return c / h(z, omegam, omegar, theta, redshift)
+
+def h0dh(z, omegam, omegar, theta):
+    return c / h(z, omegam, omegar, theta)
 
 
-def h0dm(z, omegam, omegar, theta, redshift=False):
-    return c * quad(lambda z: 1/h(z, omegam, omegar, theta, redshift), 0, z)[0]
+def h0dm(z, omegam, omegar, theta):
+    return c * quad(lambda z: 1/h(z, omegam, omegar, theta), 0, z)[0]
 
 
-def dh_over_rs(z, h0rd, omegam, omegar, theta, redshift=False):
-    return c / h0rd / h(z, omegam, omegar, theta, redshift)
+def dh_over_rs(z, h0rd, omegam, omegar, theta):
+    return c / h0rd / h(z, omegam, omegar, theta)
 
 
-def dm_over_rs(z, h0rd, omegam, omegar, theta, redshift=False):
+def dm_over_rs(z, h0rd, omegam, omegar, theta):
     z_nodes = (1 / get_x_nodes_from_theta(theta, False) - 1)[::-1]
     z_nodes = concatenate([[0], z_nodes[z_nodes < z], [z]])
     return c / h0rd * sum([
-        quad(lambda z: 1/h(z, omegam, omegar, theta, redshift), lower, upper)[0]
+        quad(lambda z: 1/h(z, omegam, omegar, theta), lower, upper)[0]
         for lower, upper in zip(z_nodes[:-1], z_nodes[1:])
     ])
 
 
-def dv_over_rs(z, h0rd, omegam, omegar, theta, redshift=False):
-    return (z * dm_over_rs(z, h0rd, omegam, omegar, theta, redshift) ** 2
-            * dh_over_rs(z, h0rd, omegam, omegar, theta, redshift)) ** (1/3)
+def dv_over_rs(z, h0rd, omegam, omegar, theta):
+    return (z * dm_over_rs(z, h0rd, omegam, omegar, theta) ** 2
+            * dh_over_rs(z, h0rd, omegam, omegar, theta)) ** (1/3)
 
 
-def dl_over_rs(zhd, zhel, h0rd, omegam, omegar, theta, redshift=False):
-    return (1+zhel) * dm_over_rs(zhd, h0rd, omegam, omegar, theta, redshift)
+def dl_over_rs(zhd, zhel, h0rd, omegam, omegar, theta):
+    return (1+zhel) * dm_over_rs(zhd, h0rd, omegam, omegar, theta)
 
 
-def dl(zhd, zhel, h0, omegam, omegar, theta, redshift=False):
-    q0 = quad(lambda z: 1/h(z, omegam, omegar, theta, redshift), 0, zhd[0])[0]
-    h_inverse = [1 / h(zi, omegam, omegar, theta, redshift) for zi in zhd]
+def dl(zhd, zhel, h0, omegam, omegar, theta):
+    q0 = quad(lambda z: 1/h(z, omegam, omegar, theta), 0, zhd[0])[0]
+    h_inverse = [1 / h(zi, omegam, omegar, theta) for zi in zhd]
     q = cumulative_trapezoid(h_inverse, zhd, initial=0) + q0
     return (1+zhel) * c / h0 * q
