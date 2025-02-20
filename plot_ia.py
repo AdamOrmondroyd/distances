471ef5f4edfab9f678c36744fb24d29b445a5b88
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-02-20 15:31:25 +0000

    many changes to plot_ia.py

--- plot_ia.py
+++ plot_ia.py
@@ -6,14 +6,17 @@ from anesthetic.samples import merge_samples_weighted
 from fgivenx import plot_lines
 from common import flexknotparamnames
 from distances import dl
+from plot import collect_chains
 from ia import df, mb, zhd, zhel, invcov, omegar
 from tqdm import tqdm
 from joblib import Parallel, delayed
 from flexknot import FlexKnot
 import smplotlib
 from pypolychord.output import PolyChordOutput
+from scipy.stats import binned_statistic
 
 
+color = '#d05a5c'
 name = sys.argv[1]
 try:
     n = int(sys.argv[2])
@@ -27,6 +30,7 @@ except IndexError:
 lcdm = read_chains(f"chains/{name}_lcdm")
 idx, ns, nss, pcs, prior = collect_chains(name, n, single)
 
+np.random.seed(1729)
 ns = ns.compress()
 
 # H0rd, Omegam, flexknot
@@ -39,8 +43,7 @@ lcdm_params = [
 params = lcdm_params + flexknotparamnames(n, tex=False)
 
 print(ns)
-ns = ns.iloc[np.argsort(ns.get_weights())]
-# ns = ns.iloc[-100:]
+# ns = ns.iloc[np.argsort(ns.get_weights())]
 print(f"{ns.columns=}")
 print(f"{params=}")
 
@@ -110,51 +113,41 @@ mb_lcdm = mb_samples(lcdm[-3:], zhd, zhel, mb, lcdm=True, parallel=False)[-1]
 print(f"{mb_lcdm=}")
 
 
-fig, axs = plt.subplots(3, gridspec_kw={'height_ratios': [3, 1, 3]},
-                        figsize=(8, 10))
+fig, axs = plt.subplots(1, 2, figsize=(9, 4), sharex=True)
 
-
-ax = axs[0]
-plot_lines(
-    lambda z, mb: mb[zhd == z],
-    zhd,
-    mb_ns,
-    weights=ns.get_weights(),
-    ax=ax,
-    cache=f"cache/{name}_{n}_mb{'_i' if single else ''}",
-    parallel=True,
-)
-ax.plot(zhd, mb_lcdm)
 
 df = df[(df['zHD'] > 0.023) | (df['IS_CALIBRATOR'] == 1)]
-#
-dfc = df[df['IS_CALIBRATOR'] == 0]
-ax.plot(dfc['zHD'], dfc['m_b_corr'], linestyle="None",
-        marker='+', markersize=0.5)
 
-# dfc = df[df['IS_CALIBRATOR'] == 1]
-# ax.plot(dfc['zHD'], dfc['m_b_corr'], linestyle="None",
-#         marker='+', markersize=0.5)
+ax = axs[0]
 
-ax.set(xscale='log', xlabel=r"$z$", ylabel=r"$m_b$")
+ahd = 1/(1+zhd)
 
-ax = axs[1]
-
-
-plot_lines(
-    lambda z, mb: mb[zhd == z] - mb_lcdm[zhd == z],
-    zhd,
+fsamps = plot_lines(
+    lambda a, mb: mb[ahd == a] - mb_lcdm[ahd == a],
+    ahd,
     mb_ns,
     weights=ns.get_weights(),
     ax=ax,
     cache=f"cache/{name}_{n}_∆mb{'_i' if single else ''}",
     parallel=True,
+    color=color,
 )
+mean = np.mean(fsamps, axis=-1)
+sigma = np.std(fsamps, axis=-1)
+ax.plot(ahd, mean, color=color, linestyle='--')
+ax.fill_between(ahd, mean-sigma, mean+sigma, color=color, alpha=0.5)
 
 dfc = df[df['IS_CALIBRATOR'] == 0]
-ax.plot(dfc['zHD'], dfc['m_b_corr']-mb_lcdm, linestyle="None",
-        marker='+', markersize=0.5)
-ax.set(xscale='log', xlabel=r"$z$", ylabel=r"$m_B-{m_B}_{\Lambda\mathrm{CDM}}$")
+ax.plot(1/(1+dfc['zHD']), dfc['m_b_corr']-mb_lcdm, linestyle="None",
+        marker='+', markersize=0.5, color='C0')
+binned_mb, bin_edges, _ = binned_statistic(1/(1+dfc['zHD']), dfc['m_b_corr']-mb_lcdm, bins=20)
+binned_a = (bin_edges[1:] + bin_edges[:-1]) / 2
+ax.stairs(binned_mb, bin_edges, color='k', label=r'binned supernovae')
+ax.axhline(0, color="C0", linestyle="--")
+ax.set(xlabel=r"$a$", ylabel=r"$m_B-{m_B}_{\Lambda\mathrm{CDM}}$",
+       # xlim=(np.min(zhd), np.max(zhd)), ylim=(-0.1, 0.1))
+       xlim=(np.min(ahd), np.max(ahd)), ylim=(-0.1, 0.1))
+ax.legend(fontsize='small', loc='upper right', frameon=True, framealpha=0.75)
 
 # dfc = df[df['IS_CALIBRATOR'] == 0]
 # ax.plot(dfc['zHD'], dfc['m_b_corr'], linestyle="None",
@@ -164,12 +157,27 @@ ax.set(xscale='log', xlabel=r"$z$", ylabel=r"$m_B-{m_B}_{\Lambda\mathrm{CDM}}$")
 # ax.plot(dfc['zHD'], dfc['m_b_corr'], linestyle="None",
         # marker='+', markersize=0.5)
 
-ax.set(xscale='log', xlabel=r"$z$", ylabel=r"$m_b - m_b(\Lambda\mathrm{CDM})$")
 
-ax = axs[2]
+ax = axs[1]
 print(f"{ns.absolute_m}")
-ns.absolute_m.plot.hist_1d(ax=ax, bins=50)
-ax.set(xlabel=r"$M_\mathrm{B}$")
+# ns.absolute_m.plot.hist_1d(ax=ax, bins=50)
+# ax.set(xlabel=r"$M_\mathrm{B}$")
+
+fk = FlexKnot(0, 1)
+
+
+def wz(a, theta):
+    theta = theta[~np.isnan(theta)]
+    return fk(a, theta)
+
+
+fsamps = plot_lines(wz, ahd, ns[params[2:]].to_numpy(), weights=ns.get_weights(), ax=ax, color=color)
+mean = np.mean(fsamps, axis=-1)
+sigma = np.std(fsamps, axis=-1)
+ax.plot(ahd, mean, color=color, linestyle='--')
+ax.fill_between(ahd, mean-sigma, mean+sigma, color=color, alpha=0.5)
+ax.axhline(-1, color="C0", linestyle="--")
+ax.set(ylim=(-3, 0), ylabel=r"$w(a)$", xlabel=r"$a$")
 fig.tight_layout()
 fig.savefig(f"plots/{name}/{name}_{n}_mb{'_i' if single else ''}.pdf", bbox_inches='tight')
 plt.show()
