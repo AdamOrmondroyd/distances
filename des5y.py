refactor supernova likelihoods to use a class to encapsulate common behaviour. Also correct DES 5Y to sort covariance matrix too

des5y.py
@@ -3,37 +3,25 @@ import numpy as np
 import pandas as pd
 from pypolychord.priors import UniformPrior
 from common import run
-from distances import dl
 from pathlib import Path
+from ia import IaLogL
 
 
 # data loading stolen from Toby
 path = Path('../clik_installs/desi/data/sn_data/DESY5')
-df = pd.read_table(path/'DES-SN5YR_HD.csv', sep=',', engine='python').sort_values("zHD")
+df = pd.read_table(path/'DES-SN5YR_HD.csv', sep=',', engine='python')
+# DES 5Y data is not sorted by redshift
+idx = np.argsort(df['zHD'])
+print(f"{idx=}")
 print(df)
-cov = np.loadtxt(path/'covsys_000.txt', skiprows=1)
+cov = np.loadtxt(path / 'covsys_000.txt', skiprows=1)
 cov = cov.reshape([-1, int(np.sqrt(len(cov)))])
-
-mb = df['MU'].to_numpy()
-zhd = df['zHD'].to_numpy()
-zhel = df['zHEL'].to_numpy()
 delta = df['MUERR_FINAL'].to_numpy()
 np.fill_diagonal(cov, delta**2 + cov.diagonal())
-one = np.ones(len(cov))[:, None]
-invcov = np.linalg.inv(cov)
-invcov_tilde = invcov - invcov @ one @ one.T @ invcov / (one.T @ invcov @ one)
-lognormalisation = 0.5 * (np.log(2*np.pi)
-                          - np.linalg.slogdet(2 * np.pi * cov)[1]
-                          - np.log((one.T @ invcov @ one).squeeze()))
+cov = cov[idx, :][:, idx]
+df = df.iloc[idx]
 
-
-def logl_des5y(h0, omegam, omegar, theta=np.array([-1])):
-    theta = np.array(theta)
-    mu = 5 * np.log10(dl(zhd, zhel, h0, omegam, omegar, theta)) + 25
-    x = mb - mu
-
-    return lognormalisation + float(-x.T @ invcov_tilde @ x / 2)
-
+logl_des5y = IaLogL(df, cov, mb_column='MU', z_cutoff=0.0)
 
 omegar = 8.24e-5
 
@@ -46,8 +34,8 @@ if __name__ == "__main__":
     ns = run(
         likelihood,
         sys.argv[1],
-        [UniformPrior(20, 100), UniformPrior(0.01, 0.99)],
+        [UniformPrior(0.01, 0.99)],
         "des5y",
-        [("H0", r"H_0"), ("Omegam", r"\Omega_\mathrm{m}")],
+        [("Omegam", r"\Omega_\mathrm{m}")],
         True,
     )

pantheon.py
@@ -1,11 +1,10 @@
 import sys
 import numpy as np
 import pandas as pd
-from numpy import log, log10
 from pypolychord.priors import UniformPrior
 from common import run
-from distances import dl, h0_dl_over_c, c
 from pathlib import Path
+from ia import IaLogL
 
 
 # data loading stolen from Toby
@@ -14,93 +13,21 @@ df = pd.read_table(path/'Pantheon+SH0ES.dat', sep=' ', engine='python')
 cov = np.loadtxt(path/'Pantheon+SH0ES_STAT+SYS.cov', skiprows=1)
 cov = cov.reshape([-1, int(np.sqrt(len(cov)))])
 
-mask = df['zHD'] > 0.023
-cepheid_mask = (df['zHD'] > 0.023) | (df['IS_CALIBRATOR'] == 1)
-ia_mask = (df['IS_CALIBRATOR'] == 0)[cepheid_mask]
-
-
-def mb_zs_mcov(df, mask):
-    mb = df['m_b_corr'].to_numpy()[mask]
-    zhd = df['zHD'].to_numpy()[mask]
-    zhel = df['zHEL'].to_numpy()[mask]
-    mcov = cov[mask, :][:, mask]
-    one = np.ones(len(mcov))[:, None]
-    invcov = np.linalg.inv(mcov)
-    invcov_tilde = invcov - invcov @ one @ one.T @ invcov / (one.T @ invcov @ one)
-    lognormalisation = 0.5 * (np.log(2*np.pi)
-                              - np.linalg.slogdet(2 * np.pi * mcov)[1]
-                              - np.log((one.T @ invcov @ one).squeeze()))
-
-    return mb, zhd, zhel, mcov, invcov, invcov_tilde, lognormalisation
-
-
-mb, zhd, zhel, mcov, invcov, invcov_tilde, lognormalisation = mb_zs_mcov(df, mask)
-mb_c, zhd_c, zhel_c, mcov_c, invcov_c, invcov_tilde_c, lognormalisation_c = mb_zs_mcov(df, cepheid_mask)
-
-
-cephdist = df['CEPH_DIST'].to_numpy()[cepheid_mask]
-delta_c = mb_c - cephdist
-cephmask = 1 - ia_mask
-delta_c_masked = delta_c * cephmask
-
-
-h0min, h0max = 20, 100
-
-# precompute some matrices
-one = np.ones(len(mcov))[:, None]
-onesigma_times_5_over_log10 = one.T @ invcov_tilde * 5 / log(10)
-new_lognormalisation = lognormalisation + log(c/(1e-5 * (h0max - h0min)))
-a = 1e-5 * h0min / c
-b = 1e-5 * h0max / c
-
-
-def logl_ia(omegam, omegar, theta=np.array([-1])):
-    theta = np.array(theta)
-    y = 5 * log10(h0_dl_over_c(zhd, zhel, omegam, omegar, theta)) - mb
-    capital_y = float((onesigma_times_5_over_log10 @ y).squeeze())
-    return (
-        - float(y.T @ invcov_tilde @ y / 2)
-        + log((b**(capital_y + 1) - a**(capital_y + 1)) / (capital_y + 1))
-        + new_lognormalisation)
-
-
-def logl_cepheid(h0, omegam, omegar, theta=np.array([-1])):
-    theta = np.array(theta)
-    mu = 5 * np.log10(dl(zhd_c, zhel_c, h0, omegam, omegar, theta)) + 25
-    x = mb_c - mu
-    x = (x * ia_mask + delta_c_masked)
-    return lognormalisation_c + float(-x.T @ invcov_tilde_c @ x / 2)
-
+logl_pantheon = IaLogL(df, cov, 'm_b_corr', z_cutoff=0.023)
 
 omegar = 8.24e-5
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 2 and "cepheid" in sys.argv[2]:
-        raise NotImplementedError("Still need to marginalise cepheid likelihood over H0")
-        def likelihood(theta):
-            h0, omegam, *theta = theta
-            return logl_cepheid(h0, omegam, omegar, theta)
+    def likelihood(theta):
+        omegam, *theta = theta
+        return logl_pantheon(omegam, omegar, theta)
 
-        ns = run(
-            likelihood,
-            sys.argv[1],
-            [UniformPrior(0.01, 0.99)],
-            "cepheid",
-            [("Omegam", r"\Omega_\mathrm{m}")],
-            True,
-        )
-
-    else:
-        def likelihood(theta):
-            omegam, *theta = theta
-            return logl_ia(omegam, omegar, theta)
-
-        ns = run(
-            likelihood,
-            sys.argv[1],
-            [UniformPrior(0.01, 0.99)],
-            "ia",
-            [("Omegam", r"\Omega_\mathrm{m}")],
-            True,
-        )
+    ns = run(
+        likelihood,
+        sys.argv[1],
+        [UniformPrior(0.01, 0.99)],
+        "ia",
+        [("Omegam", r"\Omega_\mathrm{m}")],
+        False,
+    )